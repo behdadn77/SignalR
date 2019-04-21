2ee4f578e415720c946bb12a4f2a2d636f3e26b1
Behdad Nemati <behdad.nemati1@gmail.com>
2019-04-21 19:45:44 +0430

    Add project files.

---
+++ SignalR/Hubs/ChatRoom.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR.Hubs
+{
+    public class ChatRoom : Hub
+    {
+        public async void SendMessageClientToServer(string userName, string message)
+        {
+            await Clients.All.SendAsync("SendMessageServerToClient", userName, message);
+        }
+    }
+}
